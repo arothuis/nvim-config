cf6f68126f2d889e1a8dc4569cc610662b54ca24
Alex Rothuis <alex.rothuis@gmail.com>
2025-08-09 21:20:14 +0200

    Add initial configuration

---
+++ init.lua
@@ -0,0 +1,11 @@
+-- General config
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
