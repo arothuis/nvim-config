99a6f4ff977dadcd8cbba0d7f2a96955a17ddef5
Alex Rothuis <alex.rothuis@gmail.com>
2025-09-10 22:41:52 +0200

    Update setup

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,19 @@
+return {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+  },
+  opts = {
+    presets = {
+      lsp_doc_border = true,
+      long_message_to_split = true,
+      inc_rename = true,
+    },
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = false,
+      ["vim.lsp.util.stylize_markdown"] = false,
+      ["cmp.entry.get_documentation"] = false,
+    },
+  },
+}
