31d63ab14c72b691acdf15a0196f75909db93a3b
Alex Rothuis <alex.rothuis@gmail.com>
2026-02-10 22:13:30 +0100

    Try some more plugins

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -4,11 +4,8 @@ return {
     lazy = false,
     priority = 999,
     config = {
-      "catppuccin/nvim",
-      "comfysage/evergarden",
       "EdenEast/nightfox.nvim",
       "yazeed1s/oh-lucy.nvim",
-      "rrethy/base16-nvim"
     },
   },
 }
